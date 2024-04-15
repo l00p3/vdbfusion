81c8faf67e76ceed2786e69eccc4a327dbd3a58b
Meher Malladi <meher.malladi@uni-bonn.de>
2024-04-15 22:53:24 +0200

    pybind to fetch content

--- 3rdparty/pybind11/pybind11.cmake
+++ 3rdparty/pybind11/pybind11.cmake
@@ -22,11 +22,6 @@
 
 include(FetchContent)
 FetchContent_Declare(
-  ext_pybind11
-  PREFIX pybind11
-  #TODO: Update to a release when this gets merged: https://github.com/pybind/pybind11/pull/3743
-  GIT_REPOSITORY https://github.com/pybind/pybind11
-  GIT_TAG master
-  GIT_SHALLOW ON)
-
-FetchContent_MakeAvailable(ext_pybind11)
+  pybind11
+  URL https://github.com/pybind/pybind11/archive/refs/tags/v2.12.0.tar.gz)
+FetchContent_MakeAvailable(pybind11)
